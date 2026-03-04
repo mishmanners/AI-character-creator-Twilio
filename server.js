9127bd538b8aca72f728e253064272953348b0fb
Michelle "MishManners®™" Duke <36594527+mishmanners@users.noreply.github.com>
2026-03-04 11:18:41 +1100

    generic prompt

--- server.js
+++ server.js
@@ -68,7 +68,9 @@ app.post('/message', async (req, res) => {
             fs.writeFileSync(inputImagePath, Buffer.from(base64Image.base64, "base64"));
             console.log('Saved input image:', inputImagePath); */
 
-            const PROMPT = `Create a joseim, moe anime style image of the person(s) in the photo. ALWAYS make sure the image is in PORTRAIT orientation using only dimensions "1024x1536". Always make sure the image is in portrait orientation, and if the original image is in landscape orientation, make sure the new image is in portrait orientation. Clean white background, and only white background, not transparent. Don't add any new person if the picture doesn't have it. Don't add any text overlay that could be on the original picture, unless it's something they are wearing. If the people are at the bottom of the image, fill the entire frame with the people and don't leave lots of blank space at the top. If the people are at the top of the image, fill the entire frame with the people and don't leave lots of blank space at the bottom. Don't add any extra objects in the image, only the person(s) in the original photo, and only objects that the person is wearing, holding, or interacting with in the original photo, for example if the person is sitting on a chair. If there are multiple people in the photo, make sure to include all of them in the generated image.'`;
+            // Add your prompt here and tell it what you want.
+
+            const PROMPT = `CHANGE THIS TO YOUR PROMPT. You can be as creative as you like! For example, you could say "Turn this image into a Van Gogh painting" or "Make this look like a Pixar-style character". The more specific you are, the better the results will be.`;'`;
 
             const response = await openai.responses.create({
                 model: "gpt-4o-mini",
